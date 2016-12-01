f5bc0afd76a6db3cb97b1daecf5ea037b8db88ae
x1472583690 <x14725836900@gmail.com>
2016-12-01 18:14:10 +0800

    Update testard.php

--- testard.php
+++ testard.php
@@ -9,7 +9,7 @@ mysqli_query($link,  'SET collation_connection = 'utf8_general_ci'');
 
 $sql =INSERT INTO sensorlogs (Id,Co,Tempeerature,Voltage,CreateDate)  VALUES ( NULL ,NULL ,’$_POST["sendthis"]’,’$_POST["sendthis2"]’NULL);  //新增資料
 
-//$sql =INSERT INTO sensorlogs (Tempeerature,Voltage)  VALUES ( ’$_POST["sendthis"]’,’$_POST["sendthis2"]’);  //新增資料
+$sql =INSERT INTO sensorlogs (Tempeerature,Voltage)  VALUES ( ’$_POST["sendthis"]’,’$_POST["sendthis2"]’);  //新增資料
 
 
 mysqli_query($link,$sql)or die ("無法新增".mysql_error()); //執行sql語法
